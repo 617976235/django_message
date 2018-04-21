8ded10cc4c77474d198bbe42d5256bdabcd45258
617976235 <37777023+617976235@users.noreply.github.com>
2018-04-22 00:42:51 +0800

    version 1.1 2018-4-22

--- django_message/message/views.py
+++ django_message/message/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, redirect, resolve_url
 from django.views import View
 from .models import Messages
 from .forms import MessageForm
@@ -11,9 +11,12 @@ class MessageShow(View):
       留言板信息展示和提交
     """
     def get(self, request):
-        return render(request, "message.html")
+        messages = Messages.objects.all().order_by("-id")
+        return render(request, "message.html", {"messages": messages})
 
     def post(self, request):
+        messages = Messages.objects.all().order_by("-id")
+
         get_name = request.POST.get("name")
         get_email = request.POST.get("email")
         get_address = request.POST.get("address")
@@ -22,8 +25,7 @@ class MessageShow(View):
         form = MessageForm(request.POST)
         if form.is_valid():
             Messages.objects.create(**form.clean())
-            messages = Messages.objects.all().order_by("-id")
-            return render(request, "message.html", {"messages": messages})
+            return redirect(resolve_url("messages:message"))
         else:
             form_errors = form.errors
             return render(request, "message.html", locals())
